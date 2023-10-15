5b44d3d23dc79afe88b76896aaa9e5cb97b88e2a
ylmdz <147967375+ylmdz@users.noreply.github.com>
2023-10-15 14:35:03 +0800

    creat

---
+++ config.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line
+let CONFIG_ITEMS = {
+  baseURL: 'https://api.abc338.cc',
+  qrCodeUrl: 'https://www.lsf62d.com',
+  Android_URL: 'https://d598x7s8yq448.cloudfront.net//ylm.apk',//'https://file.ylmsp.com/apk/ylm2023_61.apk',
+  IOS_URL: 'https://app.1plus.live/v2/MpI90RpLV',
+  Video_URL : '338.TV',
+  VideoEmail : 'wwwangviersans@gmail.com',
+  Telegram1_URL : 'https://t.me//yingleme_sw',//商务
+  Telegram2_URL : 'https://t.me/mayunmahuateng',//官方
+  cdnURL: 'https://file.ljnd36.com',
+}

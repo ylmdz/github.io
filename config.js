084275d408ee023a0635b3646d4c01835df330b8
ylmdz <147967375+ylmdz@users.noreply.github.com>
2023-10-15 16:24:34 +0800

    update

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 // eslint-disable-next-line
 let CONFIG_ITEMS = {
-  baseURL: 'https://api.abc338.cc',
-  qrCodeUrl: 'https://www.lsf62d.com',
-  Android_URL: 'https://d598x7s8yq448.cloudfront.net//ylm.apk',//'https://file.ylmsp.com/apk/ylm2023_61.apk',
-  IOS_URL: 'https://app.1plus.live/v2/MpI90RpLV',
-  Video_URL : '338.TV',
+  baseURL: 'https://admin.ylmydfj.com',
+  qrCodeUrl: 'https://inv08.ylmydfj.com',
+  Android_URL: 'http://155.159.255.106/yinTest.apk',
+  IOS_URL: 'https://2uyn6p9.mohufarm.net/ability/39f5hsz',
+  Video_URL : '388.TV',
   VideoEmail : 'wwwangviersans@gmail.com',
   Telegram1_URL : 'https://t.me//yingleme_sw',//商务
-  Telegram2_URL : 'https://t.me/mayunmahuateng',//官方
+  Telegram2_URL : 'https://t.me/kefu665',//官方
   cdnURL: 'https://file.ljnd36.com',
 }
